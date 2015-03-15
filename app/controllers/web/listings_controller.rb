16198015642119ae4f189568e04970a859729a20
Tian He <tianhe@gmail.com>
2015-03-15 16:09:21 -0400

    listings controller

---
+++ app/controllers/web/listings_controller.rb
@@ -0,0 +1,10 @@
+class Web::ListingsController < Web::ApplicationController
+  def index
+    @listings = Listing.order_by(created_at: 'desc')
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @listings.to_csv }
+    end    
+  end
+end
